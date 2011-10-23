068cb8ebdc92ac510115e2abe35971ee9ac0c4a6
Simon Parton <simon.parton@gmail.com>
2011-10-23 22:12:36 +0100

    implemented all code from tutorials

--- MyBot.py
+++ MyBot.py
@@ -8,37 +8,97 @@ class MyBot:
     def __init__(self):
         # define class level variables, will be remembered between turns
         pass
-    
+
     # do_setup is run once at the start of the game
     # after the bot has received the game settings
     # the ants class is created and setup by the Ants.run method
     def do_setup(self, ants):
         # initialize data structures after learning the game settings
-        pass
-    
+        self.hills = []
+        self.unseen = []
+        for row in range(ants.rows):
+            for col in range(ants.cols): self.unseen.append((row, col))
+
     # do turn is run once per turn
     # the ants class has the game state and is updated by the Ants.run method
     # it also has several helper methods to use
     def do_turn(self, ants):
         # loop through all my ants and try to give them orders
         # the ant_loc is an ant location tuple in (row, col) form
-        for ant_loc in ants.my_ants():
-            # try all directions in given order
-            directions = ('n','e','s','w')
+        orders = {}
+        targets = {}
+        def do_move_location(loc, dest):
+            directions = ants.direction(loc, dest)
             for direction in directions:
-                # the destination method will wrap around the map properly
-                # and give us a new (row, col) tuple
-                new_loc = ants.destination(ant_loc, direction)
-                # passable returns true if the location is land
-                if (ants.passable(new_loc)):
-                    # an order is the location of a current ant and a direction
-                    ants.issue_order((ant_loc, direction))
-                    # stop now, don't give 1 ant multiple orders
+                if do_move_direction(loc, direction):
+                    targets[dest] = loc
+                    return True
                     break
-            # check if we still have time left to calculate more orders
-            if ants.time_remaining() < 10:
-                break
-            
+            else:
+                return False
+
+        def do_move_direction(loc, direction):
+            new_loc = ants.destination(loc, direction)
+            if (ants.unoccupied(new_loc) and new_loc not in orders):
+                ants.issue_order((loc, direction))
+                orders[new_loc] = loc
+                return True
+            else:
+                return False
+
+        for hill_loc in ants.my_hills():
+            orders[hill_loc] = None
+
+        #first priority is finding food
+        ant_dist = []
+        for food_loc in ants.food():
+            for ant_loc in ants.my_ants():
+                dist = ants.distance(ant_loc, food_loc)
+                ant_dist.append((dist, ant_loc, food_loc))
+            ant_dist.sort()
+
+        for dist, ant_loc, food_loc in ant_dist:
+            if food_loc not in targets and ant_loc not in targets.values():
+                do_move_location(ant_loc, food_loc)
+
+        #then get of the hill
+        for hill_loc in ants.my_hills():
+            if hill_loc in ants.my_ants() and hill_loc not in orders.values():
+                for direction in ('s', 'e', 'w', 'n'):
+                    if do_move_direction(hill_loc, direction):
+                        break
+        #attack hills if one is seen
+        for hill_loc, hill_owner in ants.enemy_hills():
+            if hill_loc not in self.hills:
+                self.hills.append(hill_loc)
+
+        ant_dist = []
+        for hill_loc in self.hills:
+            for ant_loc in ants.my_ants():
+                if ant_loc not in orders.values():
+                    dist = ants.distance(ant_loc, hill_loc)
+                    ant_dist.append((dist, ant_loc))
+        ant_dist.sort()
+        for dist, ant_loc in ant_dist:
+            do_move_location(ant_loc, hill_loc)
+
+        #then explore
+        #[:] is the list copy shortcut so we're not removing from the loop 
+        #we're looping through
+        for loc in self.unseen[:]:
+            if ants.visible(loc):
+                self.unseen.remove(loc)
+        for ant_loc in ants.my_ants():
+            if ant_loc not in orders.values():
+                unseen_dist = []
+                for unseen_loc in self.unseen:
+                    dist = ants.distance(ant_loc, unseen_loc)
+                    unseen_dist.append((dist, unseen_loc))
+                unseen_dist.sort()
+                for dist, unseen_loc in unseen_dist:
+                    if do_move_location(ant_loc, unseen_loc):
+                        break
+
 if __name__ == '__main__':
     # psyco will speed up python a little, but is not needed
     try:
@@ -46,7 +106,7 @@ if __name__ == '__main__':
         psyco.full()
     except ImportError:
         pass
-    
+
     try:
         # if run is passed a class with a do_turn method, it will do the work
         # this is not needed, in which case you will need to write your own
